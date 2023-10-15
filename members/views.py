5f2e875467b0d87fbdcc205b364a5888ea8d7973
AlexeyMuratov2 <alexey.e.muratov@gmail.com>
2023-10-15 17:30:55 +0300

    add login_user

--- members/views.py
+++ members/views.py
@@ -4,4 +4,15 @@ from django.contrib import messages
 
 
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == 'POST':
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.success(request, 'Произошла ошибка. Повторите попытку.')
+            return redirect('login')
+    else:
+        return render(request, 'authenticate/login.html', {})
